074d8900d0780c527f09c348e6de965373355ce6
Woosy <dufourarthur.perso@gmail.com>
2018-12-05 10:29:09 +0100

    ajout: vérification de la branche avant execution

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-// =============================================
+// =============================================k
 // == Chargement des différents modules
 // =============================================
 
-const http = require('http')
-const crypto = require('crypto')
-const exec = require('child_process').exec
-const config = require('./config.json')
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+const config = require('./config.json');
 
 
 // =============================================
 // == Lancement du serveur
 // =============================================
-console.log('start')
 http.createServer(function (req, res) {
   req.on('data', function (chunk) {
     for (i in config.processes) {
-      if (req.url === config.processes[i].url) { // vérification de l'url
-        const sig = 'sha1=' + crypto.createHmac('sha1', config.processes[i].secret).update(chunk.toString()).digest('hex');
-        if (req.headers['x-hub-signature'] == sig) exec(config.processes[i].cmd) // vérification secret & execution déploiement
-      }
+      if (req.url !== config.processes[i].url) return; // vérification de l'url
+      if (!req.body.ref.includes(config.processes[i].branch)) return; // vérification de la branche
+      const sig = 'sha1=' + crypto.createHmac('sha1', config.processes[i].secret).update(chunk.toString()).digest('hex');
+      if (req.headers['x-hub-signature'] == sig) exec(config.processes[i].cmd) // vérification secret & execution déploiement
     }
-  })
-  res.end()
-}).listen(8081)
+  });
+  res.end();
+}).listen(8081);
