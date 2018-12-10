201a677bba026e8c8dd9e0148aeeab2bc6c15032
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-12-11 00:06:29 +0100

    rewrite: rel 1.1

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-// =============================================k
-// == Chargement des différents modules
-// =============================================
-
 const http = require('http');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
 const config = require('./config.json');
 
-
-// =============================================
-// == Lancement du serveur
-// =============================================
+/**
+ * Lancement du serveur
+ */
 http.createServer(function (req, res) {
-  req.on('data', function (chunk) {
-    for (i in config.processes) {
-      if (req.url !== config.processes[i].url) return; // vérification de l'url
-      if (!req.body.ref.includes(config.processes[i].branch)) return; // vérification de la branche
-      const sig = 'sha1=' + crypto.createHmac('sha1', config.processes[i].secret).update(chunk.toString()).digest('hex');
-      if (req.headers['x-hub-signature'] == sig) exec(config.processes[i].cmd) // vérification secret & execution déploiement
-    }
-  });
-  res.end();
-}).listen(8081);
+
+    var chunk;
+    var body = [];
+
+    req.on('data', (data) => {
+        chunk = data;
+        body.push(data);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        for (i in config.processes) {
+            if (req.url === config.processes[i].url) {
+                body = JSON.parse(decodeURIComponent(body).substr(8));
+                if (body.ref === config.processes[i].branch) {
+                    const sig = 'sha1=' + crypto.createHmac('sha1', config.processes[i].secret).update(chunk.toString()).digest('hex');
+                    if (req.headers['x-hub-signature'] == sig) {
+                        exec(config.processes[i].cmd);
+                        console.log(`Successfully deployed branch ${body.ref} from ${req.url}!`);
+                    }
+                }
+            }
+        }
+    });
+
+    res.end();
+}).listen(config.app.port);
+console.log(`================================\n=> Server started on port: ${config.app.port}`);
