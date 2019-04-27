6f051755d39ed5e2a8faedd3d54c5621b93367aa
Arthur <arthur.dufour@epsi.fr>
2019-04-27 17:27:25 +0200

    changed: linting style to standardjs

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 /* eslint-disable no-console */
-const http = require('http');
-const crypto = require('crypto');
-const { exec } = require('child_process');
-const config = require('./config.json');
+const http = require('http')
+const crypto = require('crypto')
+const { exec } = require('child_process')
+const config = require('./config.json')
 
 // gets the time in a custom format
-function addZero(i) {
-  return i < 10 ? `0${i}` : i;
+function addZero (i) {
+  return i < 10 ? `0${i}` : i
 }
-function getTime() {
-  const d = new Date();
-  const h = addZero(d.getHours());
-  const m = addZero(d.getMinutes());
-  const s = addZero(d.getSeconds());
-  return `${h}:${m}:${s}`;
+function getTime () {
+  const d = new Date()
+  const h = addZero(d.getHours())
+  const m = addZero(d.getMinutes())
+  const s = addZero(d.getSeconds())
+  return `${h}:${m}:${s}`
 }
 
 // Starting the server
 http.createServer((req, res) => {
-  let chunk;
-  let body = [];
+  let chunk
+  let body = []
 
   req.on('data', (data) => {
-    chunk = data;
-    body.push(data);
+    chunk = data
+    body.push(data)
   }).on('end', () => {
-    body = Buffer.concat(body).toString();
+    body = Buffer.concat(body).toString()
     config.processes.forEach((process) => {
       if (req.url === process.url) {
-        body = JSON.parse(decodeURIComponent(body));
+        body = JSON.parse(decodeURIComponent(body))
         if (body.ref === process.branch) {
-          const sig = `sha1=${crypto.createHmac('sha1', process.secret).update(chunk.toString()).digest('hex')}`;
+          const sig = `sha1=${crypto.createHmac('sha1', process.secret).update(chunk.toString()).digest('hex')}`
           if (req.headers['x-hub-signature'] === sig) {
-            exec(process.cmd);
-            console.log(`${getTime()} Successfully deployed branch ${body.ref} from ${req.url}!`);
+            exec(process.cmd)
+            console.log(`${getTime()} Successfully deployed branch ${body.ref} from ${req.url}!`)
           }
         }
       }
-    });
-  });
+    })
+  })
 
-  res.end();
-}).listen(config.app.port);
+  res.end()
+}).listen(config.app.port)
 
-console.log(`================================\n${getTime()} => Server started on port: ${config.app.port}`);
+console.log(`================================\n${getTime()} => Server started on port: ${config.app.port}`)
